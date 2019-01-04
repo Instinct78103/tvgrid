193ce430c6f5fb45d36c94d36314482b3b792298
Stanislav Elshaidt <media_newspaper@mail.ru>
2019-01-04 20:13:07 +0600

    Пока не работает этот файл

---
+++ php/DownloadAsWord.php
@@ -0,0 +1,40 @@
+<?
+require_once('functions.php');
+require_once('class_channel.php');
+require_once 'vendor/autoload.php';
+
+
+$channel = 'tv1000_s.txt';
+$obj = new Channel($channel);
+$text = '';
+
+
+$tvName = str_replace('.txt', '', $channel);
+$text .= $tvName . "\r\n";
+foreach($obj->raw() as $day=>$item){
+	$text .= $day . "\r\n";
+	foreach($item as $time=>$str){
+		$text .= $time . ' ' . $str . "\r\n";
+	}
+}
+
+
+
+//file_put_contents($tvName . '.txt', $text);
+$phpWord = new \PhpOffice\PhpWord\PhpWord();
+
+/* $temp = new \PhpOffice\PhpWord\TemplateProcessor('template.docx');
+$temp->setValue('channel', $text);
+$temp->saveAs("$tvName.docx"); */
+
+$doc = $phpWord->loadTemplate('template.docx');
+$section = $phpWord->createSection();
+
+$section->addText('123456');
+$section->addPageBreak();
+
+$objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
+$tttxt = $objWriter->getWriterPart('document')->getObjectAsText($section);
+
+$doc->setValue('channel', $tttxt);
+$doc->saveAs('result.docx');
