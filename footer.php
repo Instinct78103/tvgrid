80e1c2ec1e506fbe4e5bfbcc9c9656415e6dd634
Stanislav Elshaidt <media_newspaper@mail.ru>
2019-04-05 14:14:56 +0600

    header и footer стали отдельными файлами и присоединяются к index.php и list.php путем require_once()

---
+++ footer.php
@@ -0,0 +1,12 @@
+		<footer class="site-footer">
+			<nav class="site-nav">
+				<ul id="service-links">
+					<li><a href="#">Написать нам</a></li>
+				</ul>
+			</nav>
+			<h4><? echo date('Y'); ?></h4>
+		</footer>
+	</div>
+<script src="js/script.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width">
+	<title><? echo $_SERVER['SERVER_NAME']; ?></title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="container padding-5">
+		
+		<header class="site-header">
+			<h1><? echo $_SERVER['SERVER_NAME']; ?></h1>
+			<nav class="site-nav">
+				<ul id="service-links">
+					<li><a href="#">Настройки</a></li>
+					<li><a href="http://tv-grid<? echo ($_SERVER['SCRIPT_NAME'] == '/index.php') ? '/list.php' : '/index.php'; ?>">Сменить режим</a></li>
+					<li><a href="#">Войти</a></li>
+					<li><a href="#">Регистрация</a></li>
+				</ul>
+			</nav>
+		</header>
