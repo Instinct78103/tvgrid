fd2519d1be6152937196a21496c755f76f576a83
Stas <media_newspaper@mail.ru>
2019-04-10 01:24:58 +0600

    Проверка email; есть ли такой email в базе; проверка пароля

--- php/signup.php
+++ php/signup.php
@@ -9,10 +9,32 @@ if(isset($_POST['signup']))
 	{
 		$errors[] = 'Введите email!';
 	}
+	elseif(!preg_match('~[0-9a-z\._-]{2,20}@[a-z0-9\._-]{1,25}\.[a-z]{2,10}~ui', $_POST['email'])){
+		$errors[] = 'Некорректный email!';
+	}
+	else{
+		//Проверка, есть ли такой email в базе
+		$conn = new mysqli(SERVER, USER, PWORD, DB);
+		if($conn->connect_error){
+			exit('Ошибка подключения к базе: ' . $conn->connect_error);
+		}
+		$email = $_POST['email'];
+		$sql = "SELECT count(`email`) FROM `users` where `email` = '$email'";
+		
+		if($result = $conn->query($sql)){
+			if($result->fetch_row()[0] > 0){
+				$errors[] = 'Такой адрес уже зарегистрирован!';
+			}
+		}
+		$conn->close();
+	}
 	
 	if($_POST['pword'] == '')
 	{
 		$errors[] = 'Введите пароль!';
+	}
+	elseif( !preg_match('~^[а-яa-z0-9]{7,15}$~ui', $_POST['pword'])){
+		$errors[] = 'Пароль: только буквы и цифры (от 7 символов)';
 	}
 	
 	if($_POST['pword'] != $_POST['rpword'])
@@ -23,16 +45,28 @@ if(isset($_POST['signup']))
 	if(empty($errors))
 	{
 		//Все хорошо!
-		$conn = mysqli_connect(SERVER, USER, PWORD, DB);
-		if(mysqli_connect_errno()){
-			exit('Ошибка подключения к базе: ' . mysqli_connect_error());
+		$conn = new mysqli(SERVER, USER, PWORD, DB);
+		if($conn->connect_error){
+			exit('Ошибка подключения к базе: ' . $conn->connect_error);
 		}
 		
 		$email = $_POST['email'];
 		$pword = $_POST['pword'];
 		
-		$sql = "INSERT INTO `users`(`userID`, `email`, `password`) values(null, '$email', '$pword')";
-		mysqli_query($conn, $sql) or die(mysqli_error($conn));
+		$sql = "INSERT INTO `users`(`userID`, `email`, `password`) values (null, '$email', '$pword')";
+		$result = $conn->query($sql) or die($conn->error);
+		
+		$conn->close();
+		
+		echo '<div 
+		style="position: absolute;  
+		right: 20px; 
+		top: 0; 
+		padding: 30px;
+		color: green; 
+		background-color: #FFF;
+		border: 1px solid #bbbbbb;
+		border-top: none;">Успешно зарегистрирован!</div>';
 		
 	}
 	else
