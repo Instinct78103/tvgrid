31fb4d98621d693c4394920f7afceb57a96e8ffe
Stas <media_newspaper@mail.ru>
2019-04-12 17:26:15 +0600

    В Настройках выводится список таблиц и только...

--- settings.php
+++ settings.php
@@ -13,17 +13,28 @@ if($_SESSION['user']){
 		exit('Ошибка подключения к базе: ' . $conn->connect_error);
 	}
 
-	//$sql = 'SHOW TABLES';
-	$sql = 'SELECT * FROM `FindReplace`';
+	$sql = 'SHOW TABLES';
+	$sql2 = 'SELECT table_comment 
+			from information_schema.tables
+			WHERE table_schema = "TV"
+			ORDER BY table_comment ASC';
+			
+	$result = $conn->query($sql) or die($conn->error);
+	$result2 = $conn->query($sql2) or die($conn->error);
 
-	if($result = $conn->query($sql)){		
-		foreach($result->fetch_all() as $item){
-			echo $item[2] . '<br>';
+		
+		$tables = $result->fetch_all();
+		$tables_comm = $result2->fetch_all();
+		
+		echo '<ul class="tables">';
+		foreach($tables as $key=>$item){
+			echo '<li id="' . $tables[$key][0] . '">' . $tables_comm[$key][0] . '</li>';
 		}
-	}
-	else{
-		echo $conn->error;
-	}
+		echo '</ul';
+}
+else{
+	//Если пользователь не авторизован, 
+	//но оказался на этой странице
 }
 ?>
 
