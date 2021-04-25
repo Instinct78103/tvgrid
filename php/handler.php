6be88e2d449c8cebe750f0f6d841d23148c57bb6
Stanislav Elshaidt <stanislav.elshaidt@saritasa.com>
2021-04-25 22:30:09 +0600

    Добавил phpWord из vendor, теперь создаются doc-файлы в нужном виде при клике. Их останется только перенести в нужную директорию и импортировать в InDesign

--- php/handler.php
+++ php/handler.php
@@ -1,4 +1,4 @@
-<?
+<?php
 require_once('functions.php');
 $jsonStr = file_get_contents('php://input');
 $arr = json_decode($jsonStr, true);
@@ -8,6 +8,54 @@ $finalOutput = [];
 if ($arr['fileName']) {
     require_once('Channel.php');
     $obj = new Channel($arr['fileName']);
+
+    /**
+     * php-word
+     */
+
+    require_once '../vendor/autoload.php';
+    $phpWord = new \PhpOffice\PhpWord\PhpWord();
+    $section = $phpWord->addSection();
+
+    foreach (result($obj->getLinesByFileName(), false) as $line) {
+
+        if (preg_match('/^(Вторник|Среда|Четверг|Пятница|Суббота|Воскресенье)(.+)?$/ui', $line)){
+            $section->addPageBreak();
+            $section->addText($line);
+        }
+        else{
+            $section->addText(str_replace(['"', '&'], ['', '&amp;'], $line));
+        }
+
+
+//        Если только дни недели
+//        switch ($line) {
+//            case 'Вторник':
+//            case 'Среда':
+//            case 'Четверг':
+//            case 'Пятница':
+//            case 'Суббота':
+//            case 'Воскресенье':
+//                $section->addPageBreak();
+//                $section->addText($line);
+//                break;
+//            default;
+//                /**
+//                 * Удаление кавычек и "&" (& - вызывает ошибку при создании word-файла, если встречается в txt-файле)
+//                 */
+//                $section->addText(str_replace(['"', '&'], ['', '&amp;'], $line));
+//                break;
+//        }
+    }
+
+    $file = str_replace('.txt', '', $arr['fileName']) . '.docx';
+    $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord);
+    $objWriter->save($file);
+
+    /**
+     * php-word -- end
+     */
+
     $finalOutput =
         [
             'startTime' => $obj->startTime,
@@ -15,7 +63,7 @@ if ($arr['fileName']) {
             'afterDot' => $obj->afterDot,
             'lowerCase' => $obj->lowerCase,
             'raw' => join("\n", $obj->getLinesUTF8()),
-            'result' => result($obj->getLinesByFileName())
+            'result' => result($obj->getLinesByFileName()),
         ];
 } else {
     $_POST = json_decode($jsonStr, true);
@@ -24,7 +72,7 @@ if ($arr['fileName']) {
             'startTime' => $_POST['startTime'],
             'endTime' => $_POST['endTime'],
             'raw' => $_POST['txt_in'],
-            'result' => result(getLinesByJSEvent($_POST['startTime'], $_POST['endTime']))
+            'result' => result(getLinesByJSEvent($_POST['startTime'], $_POST['endTime'])),
         ];
 }
 
