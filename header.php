8112f1288d453437ae1ea78f18cef4090159aa4b
Stanislav Elshaidt <stanislav.elshaidt@saritasa.com>
2021-10-04 01:06:11 +0600

    Added 'php' before question mark

--- header.php
+++ header.php
@@ -1,29 +1,31 @@
 <!DOCTYPE html>
-<html>
+<html lang="">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width">
-	<title><? $_SERVER['SERVER_NAME'] ?></title>
-	<link rel="stylesheet" href="../css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <title><?php $_SERVER['SERVER_NAME'] ?></title>
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-	<div class="container">
-		<header class="header">
-			<h1><a href="/"><? echo $_SERVER['SERVER_NAME']; ?></a></h1>
-			<nav class="nav">
-				<ul id="service-links">
-					<? if(isset($_SESSION['user'])): ?>
-					<li><a href="/settings.php">Настройки</a></li>
-					<? endif; ?>
-				</ul>
-				<ul id="user-links">
-					<? if(isset($_SESSION['user'])): ?>
-					<li style="color: gray; font-style: italic;">Привет, <? echo explode('@', $_SESSION['user'][1])[0]; ?>!</li>
-					<li><a href="/logout.php">Выйти</a></li>
-					<? else: ?>
-					<li><a href="/login.php">Войти</a></li>
-					<li><a href="/signup.php">Регистрация</a></li>
-					<? endif; ?>
-				</ul>
-			</nav>
-		</header>
+<div class="container">
+    <header>
+        <h1><a href="/"><?php echo $_SERVER['SERVER_NAME']; ?></a></h1>
+        <nav class="nav">
+            <ul id="service-links">
+                <?php if (isset($_SESSION['user'])): ?>
+                    <li><a href="/settings.php">Настройки</a></li>
+                <?php endif; ?>
+            </ul>
+            <ul id="user-links">
+                <?php if (isset($_SESSION['user'])): ?>
+                    <li style="color: gray; font-style: italic;">
+                        Привет, <?php echo explode('@', $_SESSION['user'][1])[0]; ?>!
+                    </li>
+                    <li><a href="/logout.php">Выйти</a></li>
+                <?php else: ?>
+                    <li><a href="/login.php">Войти</a></li>
+                    <li><a href="/signup.php">Регистрация</a></li>
+                <?php endif; ?>
+            </ul>
+        </nav>
+    </header>
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?
+<?php
 session_start();
 require_once('header.php');
 
@@ -18,29 +18,30 @@ if (!isset($_POST['lowerCase'])) {
     $_POST['lowerCase'] = null;
 }
 ?>
-    <div class="left-bar">
-        <input type="time" name="startTime" id="startTime" value="08:00">
-        <input type="time" name="endTime" id="endTime" value="02:00"><br><br>
+    <div class="sidebar">
+        <div class="settings">
+            <input type="time" name="startTime" id="startTime" value="08:00">
+            <input type="time" name="endTime" id="endTime" value="02:00"><br><br>
 
-        <input type="checkbox" name="deleteReps"
-               id="deleteReps" <?php echo (isset($_POST['deleteReps'])) ? 'checked' : ''; ?>>
-        <label for="deleteReps">Удалять повторы</label><br>
+            <input type="checkbox" name="deleteReps"
+                   id="deleteReps" <?php echo (isset($_POST['deleteReps'])) ? 'checked' : ''; ?>>
+            <label for="deleteReps">Удалять повторы</label><br>
 
-        <input type="checkbox" name="deleteShortPros"
-               id="deleteShortPros" <?php echo (isset($_POST['deleteShortPros'])) ? 'checked' : ''; ?>>
-        <label for="deleteShortPros">Удалять программы менее 10 минут</label><br>
+            <input type="checkbox" name="deleteShortPros"
+                   id="deleteShortPros" <?php echo (isset($_POST['deleteShortPros'])) ? 'checked' : ''; ?>>
+            <label for="deleteShortPros">Удалять программы менее 10 минут</label><br>
 
-        <input type="checkbox" name="lowerCase"
-               id="lowerCase" <?php echo (isset($_POST['lowerCase'])) ? 'checked' : ''; ?>>
-        <label for="lowerCase">Сменить регистр</label><br>
+            <input type="checkbox" name="lowerCase"
+                   id="lowerCase" <?php echo (isset($_POST['lowerCase'])) ? 'checked' : ''; ?>>
+            <label for="lowerCase">Сменить регистр</label><br>
 
-        <input type="checkbox" name="afterDot"
-               id="afterDot" <?php echo (isset($_POST['afterDot'])) ? 'checked' : ''; ?>>
-        <label for="afterDot">Удалить часть передачи ПОСЛЕ точки или двоеточия</label><br><br>
+            <input type="checkbox" name="afterDot"
+                   id="afterDot" <?php echo (isset($_POST['afterDot'])) ? 'checked' : ''; ?>>
+            <label for="afterDot">Удалить часть передачи ПОСЛЕ точки или двоеточия</label><br><br>
 
-        <input type="number" name="changeTime" id="changeTime" value="0">
-        <label for="changeTime">Перевод времени</label><br><br>
-
+            <input type="number" name="changeTime" id="changeTime" value="0">
+            <label for="changeTime">Перевод времени</label><br><br>
+        </div>
         <div class="files"></div>
     </div>
     <div class="textarea-input">
@@ -50,4 +51,4 @@ if (!isset($_POST['lowerCase'])) {
         <textarea name="out" class="out padding-5"></textarea>
     </div>
 
-<? require_once('footer.php'); ?>
+<?php require_once('footer.php'); ?>
